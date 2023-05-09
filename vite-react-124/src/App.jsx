d63091786cbb9563d0715b6bff92e32993da0b01
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-05-09 17:18:45 +0500

    lesson-56

--- vite-react-124/src/App.jsx
+++ vite-react-124/src/App.jsx
@@ -1,11 +1,26 @@
-import Router from './components/Router';
 import './App.css';
+import Movie from './components/Movie';
+import RestCountries from './components/RestCountries';
+import { BsSearch } from 'react-icons/bs';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import Example from './components/ExampleTailwind';
 
 function App() {
 
   return (
     <>
-      <Router />
+
+      <Example />
+
+      <div className='flex'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat nisi dignissimos, cupiditate nihil soluta hic!</p>
+        <h2 className='bg-blue text-9xl border-4 border-orange-500 border-solid font-serif'>Hello Tailwind</h2>
+      </div>
+      <RestCountries />
+      <Movie />
+      <BsSearch className='search_icon' />
+      <RxHamburgerMenu />
+
     </>
   )
 }
